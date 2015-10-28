3bcc228cb4a836c3f7e22410d50117656635619a
Paul Litwin <paul@litwinconsulting.com>
2015-10-28 07:23:50 -0700

    Initial commit

---
+++ Practice3/src/Practice3/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Practice3.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
